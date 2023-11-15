5da83222b87ad751b734a7f2e57cc2c70a447599
Nikolnikoff <mikael.nikolnikov@nordsec.com>
2023-11-15 10:36:52 +0200

    install proxy

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tactics-genius/",
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.football-data.org/v4',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+ base: "/tactics-genius/",
+ plugins: [react()],
+ server: {
+  proxy: {
+    '/api': {
+      target: 'https://api.football-data.org/v4',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, ''),
     },
   },
+ },
 })
