3c9ec01733d9fbd1b6143ce490f0a2fa97e7590d
NikolnikoffTheOriginal <nikolnikoffg@gmail.com>
2024-11-12 21:16:39 +0200

    refactor: design

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,39 @@ export const App = () => {
 
   return (
     <div className="flex justify-center items-center h-[100vh]">
-      <div className="flex flex-col gap-5 shadow-lg p-40 rounded-lg">
-        <p>Welcome! Please make an appointment.</p>
+      <div className="flex flex-col gap-5 shadow-lg p-40 rounded-box bg-neutral-content">
+        <p className="self-center">Welcome! Please make an appointment.</p>
 
-        <div>
+        <div className="flex flex-col text-center">
           <p className="mb-2">Your client status</p>
           <div className="flex gap-7">
             <button
-              className={isClicked ? 'btn btn-neutral' : 'btn btn-outline'}
+              className={isClicked ? 'btn btn-neutral flex-1' : 'btn btn-outline flex-1'}
               onClick={() => setIsClicked(!isClicked)}
             >I am a new client</button>
-            <button className="btn btn-outline">I am a returning client</button>
+            <button className="btn btn-outline flex-1">I am a returning client</button>
           </div>
         </div>
 
-        <div>
+        <div className="flex flex-col text-center">
           <p className="mb-2">Your pet species</p>
           <div className="flex gap-7">
-            <button className="btn btn-outline">Dog</button>
-            <button className="btn btn-outline">Cat</button>
+            <button className="btn btn-outline flex-1">Dog</button>
+            <button className="btn btn-outline flex-1">Cat</button>
           </div>
         </div>
 
-        <div>
+        <div className="flex flex-col text-center">
           <p className="mb-2">Choose a clinician</p>
-            <select className="select select-bordered w-full max-w-xs" defaultValue={'No preference'}>
-              <option disabled>No preference</option>
-              {clinicians.map((clinician, index) => (
-                <option key={index}>{clinician}</option>
-              ))}
-            </select>
+          <select className="select select-bordered w-full" defaultValue={'No preference'}>
+            <option disabled>No preference</option>
+            {clinicians.map((clinician, index) => (
+              <option key={index}>{clinician}</option>
+            ))}
+          </select>
         </div>
+
+        <button className="btn btn-primary">Next</button>
       </div>
     </div>
   )
