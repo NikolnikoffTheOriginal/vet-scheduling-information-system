939256cbd2b515c7718219074e0e93b1bc97da5b
NikolnikoffTheOriginal <nikolnikoffg@gmail.com>
2024-11-12 19:49:27 +0200

    feat: add clinician select

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from "react"
 
 export const App = () => {
   const [isClicked, setIsClicked] = useState(false);
+  const clinicians = ['John Doe', 'Alice Smith', 'Bob Brown', 'Charlie White', 'David Black'];
 
   return (
     <div className="flex justify-center items-center h-[100vh]">
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 shadow-lg p-40 rounded-lg">
+        <p>Welcome! Please make an appointment.</p>
+
         <div>
-          <p className="mb-2">Please choose your client status</p>
+          <p className="mb-2">Your client status</p>
           <div className="flex gap-7">
             <button
               className={isClicked ? 'btn btn-neutral' : 'btn btn-outline'}
@@ -18,11 +21,21 @@ export const App = () => {
         </div>
 
         <div>
-          <p className="mb-2">Please choose your pet species</p>
+          <p className="mb-2">Your pet species</p>
           <div className="flex gap-7">
             <button className="btn btn-outline">Dog</button>
             <button className="btn btn-outline">Cat</button>
           </div>
+        </div>
+
+        <div>
+          <p className="mb-2">Choose a clinician</p>
+            <select className="select select-bordered w-full max-w-xs" defaultValue={'No preference'}>
+              <option disabled>No preference</option>
+              {clinicians.map((clinician, index) => (
+                <option key={index}>{clinician}</option>
+              ))}
+            </select>
         </div>
       </div>
     </div>
